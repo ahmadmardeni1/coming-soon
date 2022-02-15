3f23a8a639baabc8791d8625f068d5f2baefec47
ahmadmardeni1 <ahmadmardeni1@users.noreply.github.com>
2022-02-15 11:40:22 +0000

    Merge branch 'master' of https://github.com/ahmadmardeni1/coming-soon 643eede92027ec629ccdfef9a8fc0956d2900101

---
+++ precache-manifest.bac8179ac20ed77e2c7dd81a1f596b80.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc379d98f2b0fb40535429fcf8759667",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec7f50156326f979f2f2",
+    "url": "/static/css/main.f2840619.chunk.css"
+  },
+  {
+    "revision": "294dc3ec72058be3b8ca",
+    "url": "/static/js/2.f10ab0e1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.f10ab0e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec7f50156326f979f2f2",
+    "url": "/static/js/main.b33319e6.chunk.js"
+  },
+  {
+    "revision": "8cb4ec54fa2007826be9",
+    "url": "/static/js/runtime-main.05172a96.js"
+  },
+  {
+    "revision": "5159a5d89abe8bf68b09b569dbeccbc0",
+    "url": "/static/media/IBMPlexSans-Bold.5159a5d8.ttf"
+  },
+  {
+    "revision": "ee425cc83f37323665790c89758cf359",
+    "url": "/static/media/IBMPlexSans-BoldItalic.ee425cc8.ttf"
+  },
+  {
+    "revision": "40bbef08ca6f6edea2a9a9e882541ce0",
+    "url": "/static/media/IBMPlexSans-Italic.40bbef08.ttf"
+  },
+  {
+    "revision": "ee83103a4a777209b0f759a4ff598066",
+    "url": "/static/media/IBMPlexSans-Medium.ee83103a.ttf"
+  },
+  {
+    "revision": "eb7dadea8e7c37ce1a1406045dda7c1e",
+    "url": "/static/media/IBMPlexSans-MediumItalic.eb7dadea.ttf"
+  },
+  {
+    "revision": "c02b4dc6554c116e4c40f254889d5871",
+    "url": "/static/media/IBMPlexSans-Regular.c02b4dc6.ttf"
+  },
+  {
+    "revision": "baf48a32fd45986dfb5b52aada1b558a",
+    "url": "/static/media/github.baf48a32.svg"
+  },
+  {
+    "revision": "a50716dd845ea43407a9a1bcffe42aef",
+    "url": "/static/media/twitter.a50716dd.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coming-soon/precache-manifest.0c0481e2251d7a489f985028d9045799.js"
+  "/precache-manifest.bac8179ac20ed77e2c7dd81a1f596b80.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/coming-soon/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
